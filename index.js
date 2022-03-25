e82acda6ac731b02849e181d1239ffddbe9f42f7
Jamond Turner <jamondturner@yahoo.com>
2022-03-25 16:06:39 -0400

    In Progress

--- index.js
+++ index.js
@@ -1,1 +1,70 @@
-// Code your solutions in this file
+// // // Code your solutions in this file
+// // // for ([initialization)]; [conditon]; [iteration]) {
+// //     [loop body]
+
+// let books = ['Eloquent JavaScript', 'JavaScript Good Parts', 'Learn JavaScript,' ' You Js,''JavaScript the guide']
+
+// // NOTE this is an array with Five variables total within the array
+
+// let oneBook = {
+//     title: 'Eloquent JavaScript',
+//     author: 'Marijn Haverbeke',
+//     publisher: 'No Starch Press'
+// }
+
+// // NOTE this is an array which can also be looped through using the methods below.
+
+// // // function loopThroughBooks(books){
+// // for(let i= 0; i < 2; i++){
+// //     console.log(i)
+// // 
+// function loopThroughBooks(books) {
+//     for (let i = 0; i < book.length; i++) {
+//         console.log(books[i])
+//         // // books.length represents number of items in the array
+//         //     
+//         for of statement:
+//             function loopThroughBooks(books) {
+//                 for (let book of books) {
+//                     console.log(book)
+
+//                 }
+// Looping through Objects: Using a "for in" loop
+
+// loopThroughBooks(books)
+
+//                 function loopThroughObj(obj) {
+//                     for (let key in obj) {
+//                         console.log(key)
+//                         console.log(obj[key])
+//                         loopThroughObj(oneBook)
+
+// Followed below is the code along assignment.
+
+// for (let age = 30; age < 40; age++){
+//     console.log(`I'm ${age} years old.  Happy birthday to me!`);
+//     }
+
+// let cards = ["Guadalupe", "Ollie", "Aki"]
+// let eventname = ["surprise"]
+
+// function writeCards(cards, eventname) {
+
+//     for (let i = 0; i < cards.length; i++) {
+//         [`Thank you, ${cards[i]}, for the wonderful ${eventname} gift!"`]
+
+//     }
+//     // console.log(writeCards)
+
+
+function countDown(num) {
+    for (let countdown = 10; countdown > -1; countdown--) console.log(countdown);
+
+
+
+    
+}
+
+
+
+
