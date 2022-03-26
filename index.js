3ed5ef375852a3a5c09d74e8549be47658e0df4e
Jamond Turner <jamondturner@yahoo.com>
2022-03-26 15:18:49 -0400

    Assignment Complete

--- index.js
+++ index.js
@@ -1,49 +1,4 @@
-// // // Code your solutions in this file
-// // // for ([initialization)]; [conditon]; [iteration]) {
-// //     [loop body]
 
-// let books = ['Eloquent JavaScript', 'JavaScript Good Parts', 'Learn JavaScript,' ' You Js,''JavaScript the guide']
-
-// // NOTE this is an array with Five variables total within the array
-
-// let oneBook = {
-//     title: 'Eloquent JavaScript',
-//     author: 'Marijn Haverbeke',
-//     publisher: 'No Starch Press'
-// }
-
-// // NOTE this is an array which can also be looped through using the methods below.
-
-// // // function loopThroughBooks(books){
-// // for(let i= 0; i < 2; i++){
-// //     console.log(i)
-// // 
-// function loopThroughBooks(books) {
-//     for (let i = 0; i < book.length; i++) {
-//         console.log(books[i])
-//         // // books.length represents number of items in the array
-//         //     
-//         for of statement:
-//             function loopThroughBooks(books) {
-//                 for (let book of books) {
-//                     console.log(book)
-
-//                 }
-// Looping through Objects: Using a "for in" loop
-
-// loopThroughBooks(books)
-
-//                 function loopThroughObj(obj) {
-//                     for (let key in obj) {
-//                         console.log(key)
-//                         console.log(obj[key])
-//                         loopThroughObj(oneBook)
-
-// Followed below is the code along assignment.
-
-// for (let age = 30; age < 40; age++){
-//     console.log(`I'm ${age} years old.  Happy birthday to me!`);
-//     }
 
 const cards = ["Guadalupe", "Ollie", "Aki"]
 let eventName = ["surprise"]
@@ -69,5 +24,5 @@ console.log(newArray);
         for (let countdown = 10; countdown > -1; countdown--)
             console.log(countdown);
     }
-    
+
 
